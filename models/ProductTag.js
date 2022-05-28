47597f61f19b48bacdff9462bf5752160e16d137
wctestbank <wc.businessmail@gmail.com>
2022-05-28 17:11:38 -0400

    added product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,19 +2,30 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
     id: {
       type: DataTypes.INTEGER,
-      primaryKey:true,
       allowNull: false,
+      primaryKey: true,
       autoIncrement: true
     },
-    tag_name: {
-      type: DataTypes.STRING
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
     }
   },
   {
