b01cf6fac2c1666d45e00a59df63f14223ac8a76
wctestbank <wc.businessmail@gmail.com>
2022-05-28 17:53:06 -0400

    added tag get route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,19 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'price', 'stock']
+      }
+    ]
+  })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
